0342c7dba7508f5031a8738e09da3ee2bfda502c
dt5513 <datran1994@gmail.com>
2017-01-03 09:32:50 +0000

    Python Script to export pickled data to MongoDB for Jupyter Notebook usage.

---
+++ Python Scripts/pickle_to_mongo.py
@@ -0,0 +1,27 @@
+import pymongo
+import pickle
+
+tweetPath = 'C:\MetOfficeProject\\tweets_wt.pickle'
+weatherPath = 'C:\MetOfficeProject\\weather_wt.pickle'
+
+connection = pymongo.MongoClient("mongodb://localhost")
+
+with open(tweetPath,'rb') as file:
+    tweets = []
+    tweets.extend(p for p in pickle.load(file))
+
+with open(weatherPath,'rb') as file:
+    weather = []
+    weather.extend(p for p in pickle.load(file))
+
+db = connection.metoffice
+
+tweetCollection = db.tweets_wt
+tweetCollection.drop()
+for t in tweets:
+    tweetCollection.insert_one(t)
+
+weatherCollection = db.weather
+weatherCollection.drop()
+for w in weather:
+    weatherCollection.insert_one(w)
